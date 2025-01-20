ae85f22a7194c9b7adcf506d2c2185139568b097
Kazzimer <kazzimerz@gmail.com>
2025-01-20 14:00:12 -0500

    Update DestroyObject.cs

--- Symbiosis Dev/Assets/DestroyObject.cs
+++ Symbiosis Dev/Assets/DestroyObject.cs
@@ -5,7 +5,9 @@ public class scrip : MonoBehaviour
     void OnCollisionEnter(Collision collision)
     {
         if (!collision.gameObject.CompareTag("Player"))
-        Destroy(gameObject); // Destroys THIS bullet
+        {
+            Destroy(gameObject); // Destroys THIS bullet
+        }
     }
 
 }
